2cd2c2c530acecd78b61b8ae0d293f3d6f750b0a
Randy Caballero <caballerorandy6@gmail.com>
2022-10-16 14:12:27 -0500

    no image

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,23 +4,25 @@ const Result = ({ result }) => {
   const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
     result;
 
-  return (
-    <div className="md:flex mx-auto w-3/5 gap-28  md:justify-center md:items-center mt-14 text-white text-start ">
-      <img
-        className="w-44"
-        src={`https://cryptocompare.com/${IMAGEURL}`}
-        alt="crypto image"
-      />
+  console.log(IMAGEURL);
 
-      <div className="w-2/3">
-        <p className="text-3xl font-bold">
+  return (
+    <div className="container mb-6 text-center mx-auto md:flex  w-3/5 gap-28  md:justify-center md:items-center mt-6  text-white md:text-start">
+      {/* <img
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="crypto crypto"
+        className="container mx-auto block mb-6"
+      /> */}
+
+      <div className="container mx-auto w-2/3">
+        <p className="text-xl md:text-3xl font-bold">
           The price is: <span className="text-yellow-500">{PRICE}</span>
         </p>
-        <p className="text-2xl font-semibold">
+        <p className="text-md md:text-2xl font-semibold">
           Highest price of the day:{" "}
           <span className="text-yellow-500">{HIGHDAY}</span>
         </p>
-        <p className="text-xl font-semibold">
+        <p className="text-md font-semibold">
           Lowest price of the day:{" "}
           <span className="text-yellow-500">{LOWDAY}</span>
         </p>
